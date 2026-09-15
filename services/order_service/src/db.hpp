3ec8b3248ea9642bfe9603fe32cd126e3d180e2f
anna gorbatsevich <g.annu@yandex.ru>
2026-09-15 14:44:06 +0300

    Order DAO

---
+++ services/order_service/src/db.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <userver/storages/postgres/cluster.hpp>
+#include <vector>
+
+
+namespace order_service {
+struct OrderDAO {
+public:
+    OrderDAO(const userver::components::ComponentContext& context);
+    userver::v3_2_rc::storages::postgres::ResultSet GetOrders(int user_id, bool is_seller) const;
+    userver::v3_2_rc::storages::postgres::ResultSet GetBasket(int buyer_id) const;
+    void InsertOrder(userver::formats::json::Value payload) const;
+private:
+    userver::storages::postgres::ClusterPtr pg_cluster_;
+};
+} // namespace order_service 
